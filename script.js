3fe0325ee1f3a356d4a3615af013a8b229fb54ba
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2020-07-24 04:40:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-jQuery(document).ready(function(){
-    //   insert back to top button dynamicly
-     $( "#ScrollToTopButton" ).append('<a class="ScrollToTopButton" href="javascript:void(null);" style="display: none;"><i class="fa fa-angle-up"></i><iframe id="tmp_downloadhelper_iframe" style="display: none;"></iframe></a>');
-      var $window = $(window);
-      var distance = 80;
-        // scroll
-      $window.scroll(function() {
-        // header
-        if($window.scrollTop() >= distance) {
-          $(".ScrollToTopButton").fadeIn();
-        }else{
-          $(".ScrollToTopButton").fadeOut();
-        }
-      });
+// Scroll to top butoon animation
+
+jQuery(document).ready(function()
+{
+  $( "#ScrollToTopButton" ).append('<a class="ScrollToTopButton" style="display: none;"><i class="fa fa-angle-up"></i><iframe id="tmp_downloadhelper_iframe" style="display: none;"></iframe></a>');
+  
+  var $window = $(window);
+  var distance = 80;
+    
+  $window.scroll(function() 
+  {
+    if($window.scrollTop() >= distance) 
+    {
+      $(".ScrollToTopButton").fadeIn();
+    }
+    else
+    {
+      $(".ScrollToTopButton").fadeOut();
+    }
+  });
       
-      $('.ScrollToTopButton').click(function() {
-        $('html, body').animate({
-                scrollTop: 0
-            }, 800);
-     });
-    })
+  $('.ScrollToTopButton').click(function() 
+  {
+    $('html, body').animate
+    ({
+      scrollTop: 0
+    },800);
+    
+  });
+})
+
+// Drop Down Phone Navbar Animation
