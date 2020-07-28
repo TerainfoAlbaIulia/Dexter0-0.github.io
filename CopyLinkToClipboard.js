cb515fac78ea33cc2f97c846405a5a85aaaf3a87
Dexter0-0 <56682055+Dexter0-0@users.noreply.github.com>
2020-07-29 01:51:05 +0300

    Add files via upload

---
+++ CopyLinkToClipboard.js
@@ -0,0 +1,27 @@
+var clipboard = new Clipboard('.btn-copy', 
+{
+    text: function() 
+    {
+        return document.querySelector('input[type=hidden]').value;
+    }
+});
+
+clipboard.on('success', function(e) 
+{
+  alert("Copiat!");
+  e.clearSelection();
+});
+
+$("#input-url").val(location.href);
+//safari
+if (navigator.vendor.indexOf("Apple")==0 && /\sSafari\//.test(navigator.userAgent)) 
+{
+   $('.btn-copy').on('click', function() 
+   {
+    var msg = window.prompt("Copy this link", location.href);
+
+    });
+}
+
+ 
+  
